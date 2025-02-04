28a159ae6379ee88f688ba151e06f2c8845c572b
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-02-04 08:43:18 -0700

    Add project files.

---
+++ ClassNotesAndExamples/MethodExamples.vb
@@ -0,0 +1,43 @@
+﻿Option Explicit On
+Option Strict On
+
+Module MethodExamples
+
+    Sub Main()
+        Dim myLuckyNumber As Integer = 7
+        Console.WriteLine("Hello from Sub Main")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
+        'MyOtherSub()
+        'Console.WriteLine()
+        'Sandwich(myLuckyNumber)
+        Marine(myLuckyNumber)
+        Console.WriteLine("Hello from Sub Main")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
+    End Sub
+
+    Sub MyOtherSub()
+        Dim myLuckyNumber As Integer = 5
+        Console.WriteLine("Hello from My Other Sub")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
+
+
+    End Sub
+
+    Sub Sandwich(ByVal myLuckyNumber As Integer)
+        myLuckyNumber += 5
+        Console.WriteLine("Hello from Sub Sandwich")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
+    End Sub
+
+    Sub Marine(ByRef myLuckyNumber As Integer)
+        myLuckyNumber += 7
+        Console.WriteLine("Hello from Sub Marine")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        Console.WriteLine()
+    End Sub
+
+End Module
