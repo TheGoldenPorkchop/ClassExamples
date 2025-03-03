6ba3eb3de34cf4c4cd717296e0a89f5fc5a98260
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-03-03 08:49:46 -0700

    Added Text Boxes and Tab button control

--- WinFormEx/WinFormEx/WinFormEx/WinFormEx.Designer.vb
+++ WinFormEx/WinFormEx/WinFormEx/WinFormEx.Designer.vb
@@ -24,6 +24,14 @@ Partial Class WinFormEx
     Private Sub InitializeComponent()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.FirstNameLabel = New System.Windows.Forms.Label()
+        Me.FirstTextBox = New System.Windows.Forms.TextBox()
+        Me.MiddleNameTextBox = New System.Windows.Forms.TextBox()
+        Me.SecondNameLabel = New System.Windows.Forms.Label()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.ThirdNameLabel = New System.Windows.Forms.Label()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ExitButton
@@ -31,8 +39,8 @@ Partial Class WinFormEx
         Me.ExitButton.Location = New System.Drawing.Point(657, 375)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(131, 63)
-        Me.ExitButton.TabIndex = 1
-        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'UpdateButton
@@ -40,23 +48,104 @@ Partial Class WinFormEx
         Me.UpdateButton.Location = New System.Drawing.Point(520, 375)
         Me.UpdateButton.Name = "UpdateButton"
         Me.UpdateButton.Size = New System.Drawing.Size(131, 63)
-        Me.UpdateButton.TabIndex = 0
-        Me.UpdateButton.Text = "Update"
+        Me.UpdateButton.TabIndex = 4
+        Me.UpdateButton.Text = "&Update"
         Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'FirstNameLabel
+        '
+        Me.FirstNameLabel.AutoSize = True
+        Me.FirstNameLabel.Location = New System.Drawing.Point(12, 44)
+        Me.FirstNameLabel.Name = "FirstNameLabel"
+        Me.FirstNameLabel.Size = New System.Drawing.Size(72, 16)
+        Me.FirstNameLabel.TabIndex = 2
+        Me.FirstNameLabel.Text = "First Name"
+        '
+        'FirstTextBox
+        '
+        Me.FirstTextBox.Location = New System.Drawing.Point(106, 41)
+        Me.FirstTextBox.Name = "FirstTextBox"
+        Me.FirstTextBox.Size = New System.Drawing.Size(206, 22)
+        Me.FirstTextBox.TabIndex = 0
+        '
+        'MiddleNameTextBox
+        '
+        Me.MiddleNameTextBox.Location = New System.Drawing.Point(106, 69)
+        Me.MiddleNameTextBox.Name = "MiddleNameTextBox"
+        Me.MiddleNameTextBox.Size = New System.Drawing.Size(206, 22)
+        Me.MiddleNameTextBox.TabIndex = 1
+        '
+        'SecondNameLabel
+        '
+        Me.SecondNameLabel.AutoSize = True
+        Me.SecondNameLabel.Location = New System.Drawing.Point(12, 72)
+        Me.SecondNameLabel.Name = "SecondNameLabel"
+        Me.SecondNameLabel.Size = New System.Drawing.Size(88, 16)
+        Me.SecondNameLabel.TabIndex = 4
+        Me.SecondNameLabel.Text = "Middle Name"
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(106, 97)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(206, 22)
+        Me.LastNameTextBox.TabIndex = 2
+        '
+        'ThirdNameLabel
+        '
+        Me.ThirdNameLabel.AutoSize = True
+        Me.ThirdNameLabel.Location = New System.Drawing.Point(12, 100)
+        Me.ThirdNameLabel.Name = "ThirdNameLabel"
+        Me.ThirdNameLabel.Size = New System.Drawing.Size(72, 16)
+        Me.ThirdNameLabel.TabIndex = 6
+        Me.ThirdNameLabel.Text = "Last Name"
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Location = New System.Drawing.Point(106, 125)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(206, 22)
+        Me.AgeTextBox.TabIndex = 3
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Location = New System.Drawing.Point(12, 128)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(32, 16)
+        Me.AgeLabel.TabIndex = 8
+        Me.AgeLabel.Text = "Age"
         '
         'WinFormEx
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.AgeTextBox)
+        Me.Controls.Add(Me.AgeLabel)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.ThirdNameLabel)
+        Me.Controls.Add(Me.MiddleNameTextBox)
+        Me.Controls.Add(Me.SecondNameLabel)
+        Me.Controls.Add(Me.FirstTextBox)
+        Me.Controls.Add(Me.FirstNameLabel)
         Me.Controls.Add(Me.UpdateButton)
         Me.Controls.Add(Me.ExitButton)
         Me.Name = "WinFormEx"
         Me.Text = "Window Forms Example"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ExitButton As Button
     Friend WithEvents UpdateButton As Button
+    Friend WithEvents FirstNameLabel As Label
+    Friend WithEvents FirstTextBox As TextBox
+    Friend WithEvents MiddleNameTextBox As TextBox
+    Friend WithEvents SecondNameLabel As Label
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents ThirdNameLabel As Label
+    Friend WithEvents AgeTextBox As TextBox
+    Friend WithEvents AgeLabel As Label
 End Class
